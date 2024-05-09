03b0486c9492d3f967759912602144d336323eb3
typoscript <minkyu.kim.contact@gmail.com>
2024-05-09 17:59:16 +0900

    Add Member Variables

--- src/main/java/carRental/board/model/PostRequestDto.java
+++ src/main/java/carRental/board/model/PostRequestDto.java
@@ -1,5 +1,12 @@
 package carRental.board.model;
 
-public class PostRequestDto {
+import java.sql.Timestamp;
 
+public class PostRequestDto {
+	private String userId;
+	private String title;
+	private String content;
+	private boolean isNotice;
+	private Timestamp creationDate;
+	private Timestamp modificationDate;
 }
