17611a6a33c3ee2d4f731677ee906136c7120842
typoscript <minkyu.kim.contact@gmail.com>
2024-05-09 15:34:55 +0900

    Add Server Test Method

--- src/main/webapp/resources/script/car/reservation.js
+++ src/main/webapp/resources/script/car/reservation.js
@@ -1,3 +1,11 @@
-/**
- * 
- */
+$(document).ready(() => {
+	console.log("reverstaion");
+	
+	const URL = "/CarGetAllAction";
+	
+	fetch(URL, {
+		method: "GET",
+	}).then(response => response.json())
+	.then(data => console.log(data))
+	.catch(e => console.log(e));
+})
