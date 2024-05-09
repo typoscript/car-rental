bca057f063f8f8dea0254e330abafc0430eaac76
typoscript <minkyu.kim.contact@gmail.com>
2024-05-09 18:00:06 +0900

    Add Getters & Setters

--- src/main/java/carRental/board/model/PostRequestDto.java
+++ src/main/java/carRental/board/model/PostRequestDto.java
@@ -9,4 +9,34 @@ public class PostRequestDto {
 	private boolean isNotice;
 	private Timestamp creationDate;
 	private Timestamp modificationDate;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public boolean isNotice() {
+		return isNotice;
+	}
+	public void setNotice(boolean isNotice) {
+		this.isNotice = isNotice;
+	}
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+	public Timestamp getModificationDate() {
+		return modificationDate;
+	}
 }
