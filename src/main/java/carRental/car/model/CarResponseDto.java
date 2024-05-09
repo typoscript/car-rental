afcb6e1e6bfaaacc6689194a7f92041d60ef572e
typoscript <minkyu.kim.contact@gmail.com>
2024-05-09 15:59:14 +0900

    Add Member Variables

---
+++ src/main/java/carRental/car/model/CarResponseDto.java
@@ -0,0 +1,13 @@
+package carRental.car.model;
+
+public class CarResponseDto {
+	private String id;
+	private String brand;
+	private String name;
+	private String type;
+	private String fuelType;
+	private int year;
+	private String imgUrl;
+	private int fee;
+	private int mileage;
+}
