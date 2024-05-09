3037fb96b8c4a199a39db1872e3c1d52f41c3ba9
typoscript <minkyu.kim.contact@gmail.com>
2024-05-09 18:25:32 +0900

    Change File Name: PostWriteAction -> PostCreateAction

--- src/main/java/carRental/board/controller/PostCreateAction.java
+++ src/main/java/carRental/board/controller/PostCreateAction.java
@@ -15,14 +15,14 @@ import carRental.user.model.UserResponseDto;
 /**
  * Servlet implementation class PostWriteAction
  */
-@WebServlet("/PostWriteAction")
-public class PostWriteAction extends HttpServlet {
+@WebServlet("/PostCreateAction")
+public class PostCreateAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PostWriteAction() {
+    public PostCreateAction() {
         super();
         // TODO Auto-generated constructor stub
     }
