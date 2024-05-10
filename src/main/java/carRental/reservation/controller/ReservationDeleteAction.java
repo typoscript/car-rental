56a3667ae2c00cae6fcc61459b2132ffd5d9fa2f
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 17:08:25 +0900

    Modify doGet() to Handle Reservation Deletion

--- src/main/java/carRental/reservation/controller/ReservationDeleteAction.java
+++ src/main/java/carRental/reservation/controller/ReservationDeleteAction.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import carRental.reservation.model.ReservationDao;
+import carRental.reservation.model.ReservationRequestDto;
+
 /**
  * Servlet implementation class ReservationDeleteAction
  */
@@ -25,8 +28,19 @@ public class ReservationDeleteAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		ReservationDao reservationDao = ReservationDao.getInstance();
+		ReservationRequestDto reservationDto = new ReservationRequestDto();
+		
+		reservationDto.setId(id);
+		
+		if (reservationDao.deleteReservation(reservationDto)) {
+			response.sendRedirect("/myPage");
+			return;
+		}
+
+		response.sendRedirect("/myReservation");
 	}
 
 	/**
