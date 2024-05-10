83de01a3cd332d381aacf8d11a566fdb11890840
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 12:35:16 +0900

    Add Member Variables

--- src/main/java/carRental/reservation/model/ReservationDao.java
+++ src/main/java/carRental/reservation/model/ReservationDao.java
@@ -1,5 +1,12 @@
 package carRental.reservation.model;
 
-public class RservationDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class ReservationDao {
+	private static ReservationDao instance = new ReservationDao();
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 }
