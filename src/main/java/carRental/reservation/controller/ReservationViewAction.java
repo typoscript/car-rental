4aec8be9977cf698802ffeb9704d352e7c25e606
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 16:10:05 +0900

    Add cars list and reservations list

--- src/main/java/carRental/reservation/controller/ReservationViewAction.java
+++ src/main/java/carRental/reservation/controller/ReservationViewAction.java
@@ -38,6 +38,9 @@ public class ReservationViewAction extends HttpServlet {
 		CarDao carDao = CarDao.getInstance();
 		HttpSession session = request.getSession();
 		UserResponseDto user = (UserResponseDto)session.getAttribute("user");
+
+		List<ReservationResponseDto> reservations = reservationDao.findReservationAllByUserId(user.getId());
+		List<CarResponseDto> cars = carDao.findCarByUserId(user.getId());
 	}
 
 	/**
