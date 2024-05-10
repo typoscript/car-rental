1c7407687259e15a1e389ed86a2a0dd3a67f7196
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 14:53:23 +0900

    Add UTF-8 Encoding

--- src/main/java/carRental/user/controller/SignUpAction.java
+++ src/main/java/carRental/user/controller/SignUpAction.java
@@ -40,6 +40,8 @@ public class SignUpAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); 
+
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		String name = request.getParameter("name");
