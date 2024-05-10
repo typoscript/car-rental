37fac13ca953f22dd53a701a15c92522feb1da1a
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 18:03:53 +0900

    Add Variables: session & user

--- src/main/java/carRental/reservation/controller/ReservationUpdateAction.java
+++ src/main/java/carRental/reservation/controller/ReservationUpdateAction.java
@@ -1,11 +1,18 @@
 package carRental.reservation.controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import carRental.reservation.model.Reservation;
+import carRental.reservation.model.ReservationDao;
+import carRental.reservation.model.ReservationRequestDto;
+import carRental.user.model.UserResponseDto;
 
 /**
  * Servlet implementation class ReservationUpdateAction
@@ -35,8 +42,8 @@ public class ReservationUpdateAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		UserResponseDto user = (UserResponseDto)session.getAttribute("user");
 	}
 
 }
