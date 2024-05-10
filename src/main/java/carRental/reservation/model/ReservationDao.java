9f7c77f591286fe3524e7cc78b8d9c50261274e6
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 12:38:04 +0900

    Add createReservation()

--- src/main/java/carRental/reservation/model/ReservationDao.java
+++ src/main/java/carRental/reservation/model/ReservationDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import carRental.util.DBManager;
+
 public class ReservationDao {
 	private static ReservationDao instance = new ReservationDao();
 	private Connection conn;
@@ -14,5 +16,11 @@ public class ReservationDao {
 
 	public static ReservationDao getInstance() {
 		return instance;
+	}
+
+	public ReservationResponseDto createReservation(ReservationRequestDto reservationDto) {
+		String sql = "" +
+				"INSERT INTO users(user_id, car_id, start_date, end_date, status)" +
+				" VALUES(?, ?, ?, ? ,?)";
 	}
 }
