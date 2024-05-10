1f8b0eb2339961928e1ee85c10095e1a4bcadced
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 14:56:51 +0900

    Add Page Forwarding to /myReservationPage

--- src/main/java/carRental/reservation/controller/ReservationViewAction.java
+++ src/main/java/carRental/reservation/controller/ReservationViewAction.java
@@ -1,11 +1,18 @@
 package carRental.reservation.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import carRental.reservation.model.ReservationDao;
+import carRental.reservation.model.ReservationResponseDto;
+import carRental.user.model.UserResponseDto;
 
 /**
  * Servlet implementation class ReservationViewAction
@@ -25,8 +32,15 @@ public class ReservationViewAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ReservationDao reservationDao = ReservationDao.getInstance();
+		HttpSession session = request.getSession();
+		UserResponseDto user = (UserResponseDto)session.getAttribute("user");
+
+		List<ReservationResponseDto> reservations = reservationDao.findReservationAllByUserId(user.getId());
+		
+		request.setAttribute("reservations", reservations);
+
+		request.getRequestDispatcher("/myReservationPage").forward(request, response);
 	}
 
 	/**
