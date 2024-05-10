fe4317ac5766eabce0a35db3a19f250a90abaeba
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 12:32:18 +0900

    Add Constructor

--- src/main/java/carRental/reservation/model/ReservationRequestDto.java
+++ src/main/java/carRental/reservation/model/ReservationRequestDto.java
@@ -1,5 +1,25 @@
 package carRental.reservation.model;
 
-public class ReservationRequestDto {
+import java.sql.Timestamp;
+import java.time.LocalDate;
 
+public class ReservationRequestDto {
+	private int id;
+	private int userId;
+	private int carId;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private String status;
+	private Timestamp creationDate;
+
+	public ReservationRequestDto(int id, int userId, int carId, LocalDate startDate, LocalDate endDate, String status, Timestamp creationDate) {
+		this.id = id;
+		this.userId = userId;
+		this.carId = carId;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.status = status;
+		this.creationDate = creationDate;
+	}
+	
 }
