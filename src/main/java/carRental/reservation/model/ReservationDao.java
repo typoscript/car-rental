0fa415c7bd35127b289a03997936d5fef3ddb835
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 12:51:13 +0900

    Add Function: SQL Execution

--- src/main/java/carRental/reservation/model/ReservationDao.java
+++ src/main/java/carRental/reservation/model/ReservationDao.java
@@ -3,6 +3,8 @@ package carRental.reservation.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.sql.Date;
 
 import carRental.util.DBManager;
 
@@ -18,23 +20,33 @@ public class ReservationDao {
 		return instance;
 	}
 
-	public ReservationResponseDto createReservation(ReservationRequestDto reservationDto) {
+	public boolean createReservation(ReservationRequestDto reservationDto) {
 		String sql = "" +
 				"INSERT INTO rental_reservations(user_id, car_id, start_date, end_date, status)" +
 				" VALUES(?, ?, ?, ? ,?)";
 
-		ReservationResponseDto reservation = null;
+		boolean isCreated = true;
 				
 		try {
 			conn = DBManager.getConnection();
 			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, reservationDto.getUserId());
+			pstmt.setInt(2, reservationDto.getCarId());
+
+			pstmt.setDate(3, Date.valueOf(reservationDto.getStartDate()));
+			pstmt.setDate(4, Date.valueOf(reservationDto.getEndDate()));
+			pstmt.setString(5, reservationDto.getStatus());
+			
+			pstmt.execute();
 		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println("Error: ReservationDao -> createReservation()");
+			isCreated = false;
 		} finally {
 			DBManager.close(conn, pstmt);
 		}
 
-		return reservation;
+		return isCreated;
 	}
 }
