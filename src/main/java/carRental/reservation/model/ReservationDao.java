f46969eb7930bcff267aee2e65382db17b0b73a2
typoscript <minkyu.kim.contact@gmail.com>
2024-05-10 17:03:27 +0900

    Change Method Name: deleteReservation -> deleteReservationByReservationId

--- src/main/java/carRental/reservation/model/ReservationDao.java
+++ src/main/java/carRental/reservation/model/ReservationDao.java
@@ -53,7 +53,7 @@ public class ReservationDao {
 		return isCreated;
 	}
 
-	public boolean deleteReservation(ReservationRequestDto reservationDto) {
+	public boolean deleteReservationByReservationId(ReservationRequestDto reservationDto) {
 		boolean isDeleted = true;
 		String sql = "DELETE FROM rental_reservations WHERE id=?";
 
