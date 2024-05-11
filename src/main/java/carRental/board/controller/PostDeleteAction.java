7f815bb4a50709a885679673fcc509c07b2a5b0f
UndefinedSource <>
2024-05-11 16:33:50 +0900

    Add UTF-8 Encoding

--- src/main/java/carRental/board/controller/PostDeleteAction.java
+++ src/main/java/carRental/board/controller/PostDeleteAction.java
@@ -6,6 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import carRental.board.model.PostDao;
+import carRental.board.model.PostRequestDto;
+import carRental.user.model.UserResponseDto;
 
 /**
  * Servlet implementation class PostDeleteAction
@@ -32,8 +37,7 @@ public class PostDeleteAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8"); 
 	}
 
 }
