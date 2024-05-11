e9e10c0241324d0977630a631cb8b6b1867fd074
UndefinedSource <>
2024-05-11 14:20:07 +0900

    Add UTF-8 Encoding

--- src/main/java/carRental/reservation/controller/ReservationUpdateAction.java
+++ src/main/java/carRental/reservation/controller/ReservationUpdateAction.java
@@ -42,6 +42,7 @@ public class ReservationUpdateAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); 
 		HttpSession session = request.getSession();
 		UserResponseDto user = (UserResponseDto)session.getAttribute("user");
 		
