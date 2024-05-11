0fb141e4355ff76a1677a4c24dd6e3fb743140fb
UndefinedSource <>
2024-05-11 14:19:09 +0900

    Add UTF-8 Encoding

--- src/main/java/carRental/board/controller/PostCreateAction.java
+++ src/main/java/carRental/board/controller/PostCreateAction.java
@@ -37,6 +37,8 @@ public class PostCreateAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); 
+
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
 		String isNoticeChecked = request.getParameter("isNotice");
