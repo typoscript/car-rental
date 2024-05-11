23e662b5063f10d649ffcf0c2652c41c10249947
UndefinedSource <>
2024-05-11 13:59:54 +0900

    Add All Posts to 'post' Request Attribute

--- src/main/java/carRental/board/controller/BoardViewAction.java
+++ src/main/java/carRental/board/controller/BoardViewAction.java
@@ -1,11 +1,17 @@
 package carRental.board.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import carRental.board.model.PostDao;
+import carRental.board.model.PostResponseDto;
 
 /**
  * Servlet implementation class BoardViewAction
@@ -25,6 +31,11 @@ public class BoardViewAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PostDao postDao = PostDao.getInstance();
+		List<PostResponseDto> posts = postDao.findPostAll();
+		
+		request.setAttribute("posts", posts);
+
 		request.getRequestDispatcher("/boardPage").forward(request, response);
 	}
 
