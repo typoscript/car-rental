42d55378b8fbe2ea0db9311a7c090cc7c8210d79
UndefinedSource <>
2024-05-11 14:00:51 +0900

    Remove Unnecessary Imports"

--- src/main/java/carRental/board/controller/BoardViewAction.java
+++ src/main/java/carRental/board/controller/BoardViewAction.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import carRental.board.model.PostDao;
 import carRental.board.model.PostResponseDto;
