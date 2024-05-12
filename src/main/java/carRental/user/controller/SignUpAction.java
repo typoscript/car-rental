7e53a6161716ab0030c00928107ed6d89ad5cb22
UndefinedSource <>
2024-05-12 13:44:09 +0900

    Add Page Forwarding: /signUpPage

--- src/main/java/carRental/user/controller/SignUpAction.java
+++ src/main/java/carRental/user/controller/SignUpAction.java
@@ -32,8 +32,7 @@ public class SignUpAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/signUpPage").forward(request, response);
 	}
 
 	/**
