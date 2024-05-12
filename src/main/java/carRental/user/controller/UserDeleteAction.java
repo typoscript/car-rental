3c5a4fb1b184b24dce4dfa33574580fb61c30436
UndefinedSource <>
2024-05-12 16:42:56 +0900

    Add User Login Check

--- src/main/java/carRental/user/controller/UserDeleteAction.java
+++ src/main/java/carRental/user/controller/UserDeleteAction.java
@@ -31,6 +31,13 @@ public class UserDeleteAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		UserResponseDto user = (UserResponseDto)session.getAttribute("user");
+		
+		if (user == null) {
+			response.sendRedirect("/login");
+			return;
+		}
 	}
 
 	/**
