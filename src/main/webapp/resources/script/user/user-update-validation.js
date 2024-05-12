a1e0c2d50b3607e53cb9ff050bb8eb7d54748448
UndefinedSource <>
2024-05-12 14:46:12 +0900

    Add User Update Validation Methods

---
+++ src/main/webapp/resources/script/user/user-update-validation.js
@@ -0,0 +1,53 @@
+$(document).ready(() => {
+	$("form").submit(e => {
+		e.preventDefault();
+
+		if (!isValidInput())
+			return;
+		
+		e.target.submit();
+	})
+});
+
+function isValidInput() {
+	const password = $("#password").val();
+	const address = $("#address").val();
+	const phone = $("#phone").val();
+		
+	let isValid = true;
+
+	if (!isValidPassword(password)) {
+		isValid = false;
+		$(".err-msg-password").show();
+	} else {
+		$(".err-msg-password").hide();
+	}
+
+	if (!isValidAddress(address)) {
+		isValid = false;
+		$(".err-msg-address").show();
+	} else {
+		$(".err-msg-address").hide();
+	}
+
+	if (!isValidPhone(phone)) {
+		isValid = false;
+		$(".err-msg-phone").show();
+	} else {
+		$(".err-msg-phone").hide();
+	}
+	
+	return isValid;
+}
+
+function isValidPassword(password) {
+	return 10 <= password.length && password.length <= 20;	
+}
+
+function isValidAddress(address) {
+	return address.length >= 5;
+}
+
+function isValidPhone(phone) {
+	return /\d{3}-\d{4}-\d{4}/.test(phone);
+}
