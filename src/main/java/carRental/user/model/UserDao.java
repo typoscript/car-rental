3c04bf82747cd80956c72c81685f479777d064d1
UndefinedSource <>
2024-05-12 15:04:20 +0900

    Refactor isDuplPhone()

--- src/main/java/carRental/user/model/UserDao.java
+++ src/main/java/carRental/user/model/UserDao.java
@@ -261,7 +261,13 @@ public class UserDao {
 		return findUserById(id) != null;
 	}
 
-	public boolean isDuplPhone(String phone) {
-		return findUserByPhone(phone) != null;
+	public boolean isPhoneAvailable(String id, String phone) {
+		User user = findUserById(id);
+		User userWithPhone = findUserByPhone(phone);
+		
+		if (userWithPhone == null)
+			return true;
+
+		return user.equals(userWithPhone);
 	}
 }
