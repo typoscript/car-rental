21f5596f93285753a985f88bc13c3e445691203b
UndefinedSource <>
2024-05-12 22:16:28 +0900

    Add car_brands.sql

---
+++ src/main/webapp/database/car_brands.sql
@@ -0,0 +1,20 @@
+show databases;
+
+use car_rental_db;
+
+select * from car_brands;
+
+INSERT INTO car_brands(name)
+VALUES("현대");
+
+INSERT INTO car_brands(name)
+VALUES("기아");
+
+INSERT INTO car_brands(name)
+VALUES("제네시스");
+
+INSERT INTO car_brands(name)
+VALUES("르노");
+
+INSERT INTO car_brands(name)
+VALUES("테슬라");
