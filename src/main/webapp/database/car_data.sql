851922133cad123e8b38a4b6c33f9b24dd5305cf
UndefinedSource <>
2024-05-12 22:17:13 +0900

    Add car_data.sql

---
+++ src/main/webapp/database/car_data.sql
@@ -0,0 +1,76 @@
+show databases;
+
+use car_rental_db;
+
+select * from cars;
+
+-- 르노
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("르노", "SM6", "세단", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_1052.png", 150000, 1000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("르노", "QM6", "SUV", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_1064.png", 300000, 3500);
+
+
+-- 제네시스
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("제네시스", "G70", "세단", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_1107.png", 200000, 15000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("제네시스", "G80", "세단", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_1290.png", 300000, 10000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("제네시스", "GV70", "SUV", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_1161.png", 350000, 5000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("제네시스", "GV80", "SUV", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_1123.png", 400000, 1000);
+
+-- 테슬라
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("테슬라", "모델 3", "세단", "전기", 2022, "https://www.hyundai.com/contents/comparison/carimg_1129.png", 150000, 150000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("테슬라", "모델 S", "세단", "전기", 2023, "https://www.hyundai.com/contents/comparison/carimg_1154.png", 200000, 130000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("테슬라", "모델 Y", "SUV", "전기", 2024, "https://www.hyundai.com/contents/comparison/carimg_1168.png", 300000, 100000);
+
+
+-- 기아
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("기아", "레이", "경차", "가솔린", 2020, "https://www.hyundai.com/contents/comparison/carimg_1281.png", 30000, 55000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("기아", "레이 EV", "경차", "전기", 2022, "https://www.hyundai.com/contents/comparison/carimg_1267.png", 60000, 34000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("기아", "K3", "세단", "LPG", 2018, "https://www.hyundai.com/contents/comparison/carimg_1260.png", 20000, 700000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("기아", "K5", "세단", "가솔린", 2019, "https://www.hyundai.com/contents/comparison/carimg_1280.png", 30000, 500000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("기아", "카니발", "밴", "가솔린", 2022, "https://www.hyundai.com/contents/comparison/carimg_1300.png", 100000, 320000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("기아", "쏘렌토", "SUV", "가솔린", 2020, "https://www.hyundai.com/contents/comparison/carimg_1263.png", 80000, 630000);
+
+
+
+-- 현대
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("현대", "아반떼", "세단", "가솔린", 2020, "https://www.hyundai.com/contents/comparison/carimg_52.png", 30000, 300000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("현대", "아이오닉 5", "SUV", "전기", 2023, "https://www.hyundai.com/contents/comparison/carimg_1171.png", 70000, 100000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("현대", "캐스퍼", "경차", "가솔린", 2021, "https://www.hyundai.com/contents/comparison/carimg_1189.png", 35000, 450000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("현대", "쏘나타", "세단", "가솔린", 2024, "https://www.hyundai.com/contents/comparison/carimg_62.png", 40000, 50000);
+
+INSERT INTO cars(brand, name, type, fuel_type, year, img_url, fee, mileage)
+VALUES("현대", "그랜져", "세단", "하이브리드", 2024, "https://www.hyundai.com/contents/comparison/carimg_1230.png", 70000, 100000);
+
+
