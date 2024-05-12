6ef694b5881adc0cf18ca7e85f761bf6e68bae21
UndefinedSource <>
2024-05-12 21:14:35 +0900

    Add User Login Check

--- src/main/java/carRental/board/controller/PostCreateAction.java
+++ src/main/java/carRental/board/controller/PostCreateAction.java
@@ -46,15 +46,19 @@ public class PostCreateAction extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8"); 
+		HttpSession session = request.getSession();
+		UserResponseDto user = (UserResponseDto) session.getAttribute("user");
 
+		if (user == null) {
+			response.sendRedirect("/login");
+			return;
+		}
+		
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
 		String isNoticeChecked = request.getParameter("isNotice");
 
 		boolean isNotice = isNoticeChecked == null ? false : true;
-		
-		HttpSession session = request.getSession();
-		UserResponseDto user = (UserResponseDto) session.getAttribute("user");
 		
 		PostDao postDao = PostDao.getInstance();
 		PostRequestDto postDto = new PostRequestDto();
