569a2b94d1195fa22a35b8d24250aef9515d4c56
typoscript <minkyu.kim.contact@gmail.com>
2024-05-13 12:31:03 +0900

    Add Date Range Array as Response ON GET Request

--- src/main/java/carRental/reservation/controller/ReservationDateRangeView.java
+++ src/main/java/carRental/reservation/controller/ReservationDateRangeView.java
@@ -1,11 +1,20 @@
 package carRental.reservation.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import carRental.reservation.model.ReservationDao;
+import carRental.reservation.model.ReservationRequestDto;
+import carRental.reservation.model.ReservationResponseDto;
 
 /**
  * Servlet implementation class ReservationDateRangeView
@@ -26,8 +35,25 @@ public class ReservationDateRangeView extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String carId = request.getParameter("carId");
+		ReservationRequestDto reservationDto = new ReservationRequestDto();
+		reservationDto.setCarId(Integer.parseInt(carId));
+
+		List<ReservationResponseDto> dateRanges = ReservationDao.getInstance().findReservationDateRangesById(reservationDto);
+		
+		JSONArray dateRangeArr = new JSONArray();
+		
+		for (ReservationResponseDto dateRange : dateRanges) {
+			JSONObject dateObject = new JSONObject();
+			dateObject.put("startDate", dateRange.getStartDate());
+			dateObject.put("endDate", dateRange.getStartDate());
+			
+			dateRangeArr.put(dateObject);
+		}
+
+		response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+	    response.getWriter().write(dateRangeArr.toString());
 	}
 
 	/**
